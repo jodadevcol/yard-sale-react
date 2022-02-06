2b3347a1c60ba41a886457e8dc182545a9c8ecd2
Daniel García <dagardev.col@gmail.com>
2022-02-06 12:23:45 -0500

    [ADD] Pages Order

---
+++ src/pages/Order.js
@@ -0,0 +1,22 @@
+import ListOrder from "../containers/ListOrder/ListOrder"
+import "./Order.css"
+
+function Order() {
+  return (
+    <section className="my-order-main">
+      <div className="my-order-container">
+        <h1 className="my-order-title">My order</h1>
+        <div className="resume-order">
+          <p className="resume-info">
+            <span>04.25.2021</span>
+            <span>6 articles</span>
+          </p>
+          <p className="resume-price">$ 560.00</p>
+        </div>
+        <ListOrder />
+      </div>
+    </section>
+  )
+}
+
+export default Order
