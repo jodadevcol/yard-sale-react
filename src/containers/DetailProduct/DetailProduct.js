d41826b7f300b6a0272f519bd7c5674a034c2e8c
Daniel García <dagardev.col@gmail.com>
2022-02-06 12:21:09 -0500

    [ADD] Container Detail Produt

---
+++ src/containers/DetailProduct/DetailProduct.js
@@ -0,0 +1,14 @@
+import ResumeProduct from "../../components/ResumeProduct/ResumeProduct"
+import "./DetailProduct.css"
+
+function DetailProduct() {
+  return (
+    <section className="detail-product-main">
+      <div className="detail-product-container">
+        <ResumeProduct />
+      </div>
+    </section>
+  )
+}
+
+export default DetailProduct
