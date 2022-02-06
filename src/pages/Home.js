d22693bbfb51b2ec49737b48439e6288ee706e9d
Daniel García <dagardev.col@gmail.com>
2022-02-06 12:24:24 -0500

    [ADD] Pages Home, Not Found and My Orders

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import ListProducts from "../containers/ListProducts/ListProducts"
+
+
+function Home() {
+  return (
+    <>
+      <ListProducts />
+    </>
+  )
+}
+
+export default Home
---
+++ src/pages/NotFound.js
@@ -0,0 +1,7 @@
+function NotFound() {
+  return (
+    <h1> Pagina no encntrada</h1>
+  )
+}
+
+export default NotFound
