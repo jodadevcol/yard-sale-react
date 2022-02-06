9ba0ec6106e787052aed945906121e3bdf760238
Daniel García <dagardev.col@gmail.com>
2022-02-06 12:22:46 -0500

    [ADD] Container List Orders

---
+++ src/containers/ListOrders/ListOrders.js
@@ -0,0 +1,10 @@
+import ResumeOrders from "../../components/ResumeOrders/ResumeOrders"
+import "./ListOrders.css"
+
+function ListOrders() {
+  return (
+    <ResumeOrders />
+  )
+}
+
+export default ListOrders
