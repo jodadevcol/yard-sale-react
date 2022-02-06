c19a33430e70e917da1a1904a19878f271ae6e5a
Daniel García <dagardev.col@gmail.com>
2022-02-06 12:22:31 -0500

    [ADD] Container List Order

---
+++ src/containers/ListOrder/ListOrder.js
@@ -0,0 +1,12 @@
+import ResumeOrder from "../../components/ResumeOrder/ResumeOrder"
+import "./ListOrder.css"
+
+function ListOrder() {
+  return (
+    <div className="shopping-container">
+      <ResumeOrder />
+    </div>
+  )
+}
+
+export default ListOrder
