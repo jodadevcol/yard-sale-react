0455549d13bda4f12bcc8a76535700f5fe682ff9
Daniel García <dagardev.col@gmail.com>
2022-02-06 12:22:12 -0500

    [MODIFIED] Index and App of Application

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom"
+import Layout from "./containers/Layout/Layout";
+import Home from "./pages/Home";
 import Login from "./containers/Login/Login"
-import RecoveryPass from "./containers/Recovery/RecoveryPass";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import MyAccount from "./pages/MyAccount"
+import MyAccountCreated from "./pages/MyAccountCreated"
+import MyOrders from "./pages/MyOrders"
+import Order from "./pages/Order"
+import Orders from "./pages/Orders"
+import SendEmail from "./pages/SendEmail"
+import RecoveryPass from "./containers/Recovery/RecoveryPass"
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Routes>
-        <Route path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/recovery-password" component={RecoveryPass} />
-        <Route component={NotFound} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="recovery-password" element={<RecoveryPass />} />
+          <Route path="account" element={<MyAccount />} />
+          <Route path="account-created" element={<MyAccountCreated />} />
+          <Route path="my-orders" element={<MyOrders />} />
+          <Route path="order" element={<Order />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="send-email" element={<SendEmail />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
