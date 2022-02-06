28efe56bf4c75a2751b6eba59aadf6c9adc65ad8
Daniel García <dagardev.col@gmail.com>
2022-02-06 12:20:21 -0500

    [ADD] Component Resume Orders

---
+++ src/components/ResumeOrders/ResumeOrders.js
@@ -0,0 +1,16 @@
+import "./ResumeOrders.css"
+
+function ResumeOrders() {
+  return (
+    <div className="resume-orders">
+      <p className="resume-info">
+        <span>04.25.2021</span>
+        <span>6 articles</span>
+      </p>
+      <p className="resume-price">$ 560.00</p>
+      <span className="resume-icon icon"></span>
+    </div>
+  )
+}
+
+export default ResumeOrders
