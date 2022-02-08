24d2e99fa6e7016e34e893d8edc6ee5ebe4cd519
Daniel García <dagardev.col@gmail.com>
2022-02-08 12:27:08 -0500

    [ADD, MODIFIED] Files configurations

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@containers': path.resolve(__dirname, 'src/containers'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@icons': path.resolve(__dirname, 'src/assets/icons'),
+      '@img': path.resolve(__dirname, 'src/assets/img')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          "style-loader",
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        type: 'asset'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    })
+  ],
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+    hot: true,
+    historyApiFallback: true,
+  },
+}
