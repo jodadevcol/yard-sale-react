9114a6c6a26b50b29fcab0e43221ad456395a0a5
Daniel García <dagardev.col@gmail.com>
2022-02-10 22:24:44 -0500

    [ADD] Icon, arrow functions view total of shopping

--- src/containers/ShoppingCart/index.js
+++ src/containers/ShoppingCart/index.js
@@ -6,23 +6,43 @@ import BtnPrimary from "@components/Buttons/Primary"
 import AppContext from "@contexts/AppContext"
 /* Styles & Sources */
 import "./styles.css"
+import IconArrowLeft from "@icons/arrow-left.svg"
+
+const styleIcon = {
+  backgroundImage: `url(${IconArrowLeft})`,
+}
 
 function ShoppingCart() {
   const { state } = useContext(AppContext)
+
+  const totalArticles = () => {
+    const total = state.cart.length
+
+    return `${total} Articles`
+  }
+  const totalShopping = () => {
+    const reducer = (valueAccumulator, valueCurrent) => valueAccumulator + valueCurrent.price
+    const sumShopping = state.cart.reduce(reducer, 0)
+
+    return sumShopping
+  }
 
   return (
     <div className="shopping-cart-container">
       <aside className="shopping-cart">
         <div className="shopping-cart-title">
-          <span className="arrow-left icon"></span>
+          <span
+            className="icon arrow-left"
+            style={styleIcon}
+          ></span>
           <h2 className="shopping-title">Shopping cart</h2>
         </div>
         <div className="shopping-container">
           {
-            state.cart.map(itemShopping => (
+            state.cart.map((itemShopping, index) => (
               <ItemShopping
+                key={`item-${itemShopping.id}-shopping-0${index}`}
                 itemShopping={itemShopping}
-                key={`item-${itemShopping.id}-shopping`}
               />
             ))
           }
@@ -31,9 +51,9 @@ function ShoppingCart() {
         <div className="resume-order">
           <p className="resume-info">
             <span>Total</span>
-            <span>6 articles</span>
+            <span>{totalArticles()}</span>
           </p>
-          <p className="resume-price">$ 560.00</p>
+          <p className="resume-price">$ {totalShopping()}</p>
         </div>
 
         <BtnPrimary
