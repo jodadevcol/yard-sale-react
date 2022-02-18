e33ea199f070e3db1b538b21554b22476758c573
Daniel García <dagardev.col@gmail.com>
2022-02-17 22:06:55 -0500

    [UPDATED]  Config WebPack, Alias, devServer

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,31 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path")
+const webpack = require("webpack")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/'
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
   },
-  mode: 'development',
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"],
     alias: {
-      '@img': path.resolve(__dirname, 'src/assets/img'),
-      '@icons': path.resolve(__dirname, 'src/assets/icons'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@containers': path.resolve(__dirname, 'src/containers'),
-      '@layouts': path.resolve(__dirname, 'src/layouts'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@services': path.resolve(__dirname, 'src/services'),
-      '@contexts': path.resolve(__dirname, 'src/contexts'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-    }
+      /* Sources */
+      img: path.resolve(__dirname, "src/assets/img"),
+      icons: path.resolve(__dirname, "src/components/Icons"),
+      /* Architecture */
+      components: path.resolve(__dirname, "src/components"),
+      containers: path.resolve(__dirname, "src/containers"),
+      pages: path.resolve(__dirname, "src/pages"),
+      routes: path.resolve(__dirname, "src/routes"),
+      /* API & Helpers */
+      services: path.resolve(__dirname, "src/services"),
+      contexts: path.resolve(__dirname, "src/contexts"),
+      hooks: path.resolve(__dirname, "src/hooks"),
+    },
   },
   module: {
     rules: [
@@ -29,39 +33,57 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: 'html-loader'
-          }
-        ]
-      },
-      {
-        test: /\.(css|scss)$/,
-        use: [
-          "style-loader",
-          "css-loader",
+            loader: "html-loader",
+          },
         ],
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
-        type: 'asset'
-      }
-    ]
+        test: /\.css$/,
+        use: [{ loader: MiniCssExtractPlugin.loader }, "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: { name: "assets/[hash].[ext]" },
+          },
+        ],
+      },
+      {
+        test: /\.(mp4|m4v)$/,
+        use: "file-loader?name=videos/[name].[ext]",
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: './index.html'
-    })
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProvidePlugin({
+      React: "react",
+    }),
   ],
   devServer: {
     static: path.resolve(__dirname, "./dist"),
-    hot: true,
     historyApiFallback: true,
+    compress: true,
+    port: 4545,
+    hot: true,
+    client: {
+      reconnect: false,
+    },
   },
 }
