048877f20a7a7315e50ff051e54be23842980490
Daniel García <dagardev.col@gmail.com>
2022-02-17 22:15:03 -0500

    [ADD] Services of Get All Products

---
+++ src/services/getAllProducts.js
@@ -0,0 +1,21 @@
+const URL_API = `https://api.escuelajs.co/api/v1/products`
+
+const dataStructure = (dataAPI) => {
+  if (Array.isArray(dataAPI)) {
+    const products = dataAPI.map((product) => {
+      const { id, title, price } = product
+      const image = product.images[0]
+
+      return { id, title, price, image }
+    })
+
+    return products
+  }
+  return []
+}
+
+export function getAllProducts() {
+  return fetch(URL_API)
+    .then((response) => response.json())
+    .then(dataStructure)
+}
