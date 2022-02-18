3755b42a2e7f70cc0466cf4c792191d9fb98bea5
Daniel García <dagardev.col@gmail.com>
2022-02-17 22:14:26 -0500

    [UPDATED] Structure of file

--- src/components/ItemShopping/index.js
+++ src/components/ItemShopping/index.js
@@ -1,13 +1,9 @@
 import React, { useContext } from "react"
 /* Contexts */
-import AppContext from "@contexts/AppContext"
+import AppContext from "contexts/AppContext"
 /* Styles & Sources */
 import "./styles.css"
-import IconClose from "@icons/icon_close.png"
-
-const styleIcon = {
-  backgroundImage: `url(${IconClose})`
-}
+import { IconClose } from "icons/IconClose"
 
 function ItemShopping({ itemShopping, index }) {
   const { removeToCart } = useContext(AppContext)
@@ -21,18 +17,21 @@ function ItemShopping({ itemShopping, index }) {
       <figure className="shopping-picture">
         <img
           className="picture-img"
-          src={itemShopping.images[0]}
+          src={itemShopping.image}
           alt={itemShopping.title}
         />
       </figure>
       <div className="shopping-detail">
         <p>{itemShopping.title}</p>
         <p>$ {itemShopping.price}</p>
-        <span
+        {/* <span
           className="delet-product icon"
           style={styleIcon}
           onClick={() => hanldeRemove(itemShopping, index)}
-        ></span>
+        ></span> */}
+        <span>
+          <IconClose />
+        </span>
       </div>
     </div>
   )
