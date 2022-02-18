f36cfab492ec2d7a5fef1374ec43700cc166c11e
Daniel García <dagardev.col@gmail.com>
2022-02-17 22:15:19 -0500

    [ADD] Create files of Routes of App

---
+++ src/routes/index.js
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+/* Pages */
+import Home from "pages/Home"
+import NotFound from "pages/NotFound"
+/* Componentes */
+import Main from "containers/Main"
+
+export const MainRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route index element={<Home />} />
+          {/* <Route path="login" element={<Login />} />
+          <Route path="newpassword" element={<NewPassword />} />
+          <Route path="recovery-password" element={<RecoveryPass />} />
+          <Route path="account" element={<MyAccount />} />
+          <Route path="account-created" element={<MyAccountCreated />} />
+          <Route path="my-orders" element={<MyOrders />} />
+          <Route path="order" element={<Order />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="send-email" element={<SendEmail />} /> */}
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
