84770052479683f178d53398b14bf5b4341da1de
Daniel García <dagardev.col@gmail.com>
2022-02-24 19:59:21 -0500

    [ADD] New Icon Search of component

---
+++ src/components/Icons/IconSearch.jsx
@@ -0,0 +1,15 @@
+export const IconSearch = ({className, fill = '#C7C7C7'}) => (
+  <svg
+    className={className}
+    fill="none"
+    viewBox="0 0 17 17"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill={fill}
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M12.798 10.995a.71.71 0 0 1-.098-.826s.069-.092.211-.383a6.8 6.8 0 1 0-3.135 3.13c.294-.144.388-.213.388-.213a.714.714 0 0 1 .83.095l3.83 3.829a1.275 1.275 0 0 0 1.803-1.804l-3.83-3.828ZM11.05 6.8a4.25 4.25 0 1 1-8.5 0 4.25 4.25 0 0 1 8.5 0Z"
+    />
+  </svg>
+)
